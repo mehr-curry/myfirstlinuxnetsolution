25a2746608635d23ac45f35fb75378ef0a00d8e5
merc <merc@suigin.local>
2018-08-13 10:02:18 +0200

    First change

--- myfirstlinuxnetapp/Program.cs
+++ myfirstlinuxnetapp/Program.cs
@@ -7,6 +7,7 @@ namespace myfirstlinuxnetapp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World again!");
             Console.ReadLine();
         }
     }
